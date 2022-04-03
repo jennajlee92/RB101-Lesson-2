319365704de10464347ded4030fbd50b62e84208
Jenna <55537982+jennajlee92@users.noreply.github.com>
2022-04-03 15:08:39 -0700

    Added pseudocode practice

--- pseudo.rb
+++ pseudo.rb
@@ -40,3 +40,41 @@ def everyOther(array)
 end
 
 p everyOther([1,4,7,2,5]) # => [1,7,5]
+
+# returns index of 3rd occurrence of a given character in a string
+
+def occurrence(char, string)
+  idx = nil
+  iter = 0
+  count = 0
+  until count == 3 || iter == string.length
+    if string[iter] == char
+      count += 1
+    end
+    iter += 1
+  end
+  if count == 3
+    idx = iter - 1
+  end
+  idx
+end
+
+p occurrence('x', 'axbxcdxex') # => 6
+
+# return result of merging two arrays of numbers, with first array in the even position and second in the odd
+
+def merge(first_array, second_array)
+  final_array = []
+  iter = 0
+  while second_array != []
+    if iter == 0 || iter % 2 == 0
+      final_array.push(first_array.shift)
+    else
+      final_array.push(second_array.shift)
+    end
+    iter += 1
+  end
+  final_array
+end
+
+p merge([1, 2, 3], [4, 5, 6]) # => [1, 4, 2, 5, 3, 6]
