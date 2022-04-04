4c4c9f43151d5d249dc7bb465af9cea6df6e677b
Jenna <55537982+jennajlee92@users.noreply.github.com>
2022-04-03 19:45:03 -0700

    Refactored calculator

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,74 @@
-Kernel.puts("Welcome to Calculator!")
-Kernel.puts("Input a number:")
-num1 = Kernel.gets().chomp()
-Kernel.puts("Input another number:")
-num2 = Kernel.gets().chomp()
-Kernel.puts("What operation do you want to perform on these numbers? (+ - / *)")
-op = Kernel.gets().chomp()
-
-unless op == "+" || op == "-" || op == "/" || op == "*"
-  Kernel.puts("Oops! Not a valid operation. Try again.")
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-case op
+answer = nil
+op = nil
+num1 = nil
+num2 = nil
+
+prompt("Welcome to Calculator!")
+
+loop do
+loop do
+  prompt("Input a number:")
+  num1 = Kernel.gets().chomp()
+
+  if num1.to_i != 0
+    break
+  else
+    prompt("That's not a valid number!")
+  end
+end
+
+loop do
+  prompt("Input another number:")
+  num2 = Kernel.gets().chomp()
+
+  if num2.to_i != 0
+    break
+  else
+    prompt("That's not a valid number!")
+  end
+end
+
+loop do
+  prompt("What operation do you want to perform on these numbers? (+ - / *)")
+  op = Kernel.gets().chomp()
+
+  if op == "+" || op == "-" || op == "/" || op == "*"
+    break
+  else
+    prompt("That's not a valid operation!")
+  end
+end
+
+result = case op
 when "+"
-  result = num1.to_i + num2.to_i
+  num1.to_i + num2.to_i
 when "-"
-  result = num1.to_i - num2.to_i
+  num1.to_i - num2.to_i
 when "/"
-  result = num1.to_f / num2.to_f
+  num1.to_f / num2.to_f
 when "*"
-  result = num1.to_i * num2.to_i
+  num1.to_i * num2.to_i
 end
 
 Kernel.puts("#{num1} #{op} #{num2} equals #{result}.")
+
+loop do
+  prompt("Do you want to perform another operation? (y/n)")
+  answer = Kernel.gets().chomp()
+
+  if answer == 'y' || answer == 'n'
+    break
+  else
+    prompt("That's not a valid answer!")
+  end
+end
+
+if answer == 'n'
+  break
+end
+
+end
